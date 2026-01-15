3f891b90d7f38d9e997e15423fdfd197f7e911a6
Miguel Quispe <dev@miguelquispe.com>
2026-01-15 12:16:50 -0500

    chore(frontend): enable typescript alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@/pages': path.resolve(__dirname, './src/pages'),
+      '@/features': path.resolve(__dirname, './src/features'),
+      '@/api': path.resolve(__dirname, './src/api'),
+    }
+  }
 })
